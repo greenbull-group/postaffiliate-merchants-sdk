efac1a222a7f70306cea64d64c2d6d81dde1161c
Oussama Harzallah <oharzallah@greenbull.group>
2020-09-04 11:06:53 +0200

    Enh: Load two instance for PostAffiliate

--- build/postaffiliate.js
+++ build/postaffiliate.js
@@ -1,7 +1,11 @@
 "use strict";
 
 const PostAffiliatePro = require("./class/postaffiliate"); //Export singleton instance
+//module.exports = new PostAffiliatePro();
 
 
-module.exports = new PostAffiliatePro();
+module.exports = {
+  arya: new PostAffiliatePro(),
+  enfinrentable: new PostAffiliatePro()
+};
 //# sourceMappingURL=postaffiliate.js.map
--- src/postaffiliate.js
+++ src/postaffiliate.js
@@ -1,4 +1,8 @@
 const PostAffiliatePro = require("./class/postaffiliate");
 
 //Export singleton instance
-module.exports = new PostAffiliatePro();
+//module.exports = new PostAffiliatePro();
+module.exports = {
+  arya: new PostAffiliatePro(),
+  enfinrentable: new PostAffiliatePro(),
+};
